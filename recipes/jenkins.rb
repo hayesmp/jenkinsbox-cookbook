b28cc56cb6c1f6ba707843235a6dc7019c3b53b6
Michael Hayes <tasslehawf@mac.com>
2013-09-05 19:20:52 -0500

    include_recipe "apt"

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -4,36 +4,33 @@
 #
 # Install and setup Jenkins environment
 #
-
+include_recipe "apt"
 include_recipe "jenkins"
 
-def install_github_jenkins_plugin
-  jenkins_cli "install-plugin github"
+%w{ git github }.each do |plugin|
+  jenkins_cli "install-plugin #{plugin}"
   jenkins_cli "safe-restart"
 end
 
-install_github_jenkins_plugin
 
 #node["rackbox"]["jenkins"]["git_repo"]
 #node["rackbox"]["jenkins"]["command"]
 #node["rackbox"]["jenkins"]["job"]
 
 
-=begin
-git_branch = 'master'
-job_name = node["rackbox"]["jenkins"]["job"]
-
-job_config = File.join(node[:jenkins][:node][:home], "#{job_name}-config.xml")
-
-jenkins_job job_name do
-  action :nothing
-  config job_config
-end
-
-template job_config do
-  source "sigar-jenkins-config.xml"
-  variables :job_name => job_name, :branch => git_branch, :node => node[:fqdn]
-  notifies :update, resources(:jenkins_job => job_name), :immediately
-  notifies :build, resources(:jenkins_job => job_name), :immediately
-end
-=end
+#git_branch = 'master'
+#job_name = node["rackbox"]["jenkins"]["job"]
+#
+#job_config = File.join(node[:jenkins][:node][:home], "#{job_name}-config.xml")
+#
+#jenkins_job job_name do
+#  action :nothing
+#  config job_config
+#end
+#
+#template job_config do
+#  source "sigar-jenkins-config.xml"
+#  variables :job_name => job_name, :branch => git_branch, :node => node[:fqdn]
+#  notifies :update, resources(:jenkins_job => job_name), :immediately
+#  notifies :build, resources(:jenkins_job => job_name), :immediately
+#end
